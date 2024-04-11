1dc743e312feb91cdcafa3a92fae2c4459dac645
Ambuj Kumar <ambuj.k@subcom.tech>
2024-04-11 10:32:25 +0530

    update home row mods

--- keyboards/sofle/keymaps/that-ambuj/keymap.c
+++ keyboards/sofle/keymaps/that-ambuj/keymap.c
@@ -120,8 +120,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
   KC_GRV,   KC_1,           KC_2,           KC_3,               KC_4,           KC_5,                   KC_6,    KC_7,      KC_8,    KC_9,    KC_0,  KC_GRV,
   KC_ESC,   KC_Q,           KC_W,           KC_E,               KC_R,           KC_T,                   KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,  KC_BSPC,
-  KC_TAB,   LCTL_T(KC_A),   LALT_T(KC_S),   LGUI_T(KC_D),       LSFT_T(KC_F),   KC_G,                   KC_H,    RSFT_T(KC_J),      RGUI_T(KC_K),  LALT_T(KC_L), RCTL_T(KC_SCLN),  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,                                   KC_HOME,  KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+  LCTL_T(KC_TAB),   LCTL_T(KC_A),   LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,                   KC_H,    RSFT_T(KC_J),      RGUI_T(KC_K),  LALT_T(KC_L), RCTL_T(KC_SCLN),  RCTL_T(KC_QUOT),
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,                                           KC_HOME,  KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LGUI,KC_LALT,KC_LCTL, MO(2), KC_ENT,                                        KC_SPC,  MO(3), KC_RCTL, KC_RALT, KC_RGUI
 ),
 /*
