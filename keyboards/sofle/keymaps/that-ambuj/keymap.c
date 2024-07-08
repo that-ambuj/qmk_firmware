6fcd03ebac57ad4871255453ae05d1319fbbe406
Ambuj Kumar <ambuj.k@subcom.tech>
2024-07-08 10:23:27 +0530

    change encoder click keymaps

--- keyboards/sofle/keymaps/that-ambuj/keymap.c
+++ keyboards/sofle/keymaps/that-ambuj/keymap.c
@@ -120,8 +120,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
   KC_GRV,   KC_1,           KC_2,           KC_3,               KC_4,           KC_5,                   KC_6,    KC_7,      KC_8,    KC_9,    KC_0,  KC_GRV,
   KC_ESC,   KC_Q,           KC_W,           KC_E,               KC_R,           KC_T,                   KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,  KC_BSPC,
-  LCTL_T(KC_TAB),   LCTL_T(KC_A),   LALT_T(KC_S),   LGUI_T(KC_D),   LSFT_T(KC_F),   KC_G,                   KC_H,    RSFT_T(KC_J),      RGUI_T(KC_K),  LALT_T(KC_L), RCTL_T(KC_SCLN),  RCTL_T(KC_QUOT),
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,                                           KC_HOME,  KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+  LCTL_T(KC_TAB),   LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,                   KC_H,    RSFT_T(KC_J),      RCTL_T(KC_K),  LALT_T(KC_L), RGUI_T(KC_SCLN),  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,                                           KC_HOME,  KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LGUI,KC_LALT,KC_LCTL, MO(2), KC_ENT,                                        KC_SPC,  MO(3), KC_RCTL, KC_RALT, KC_RGUI
 ),
 /*
@@ -186,7 +186,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   TO(0), TO(1) , _______ , _______ , _______ , _______,                           _______,  _______  ,  DT_PRNT ,  DT_DOWN , DT_UP , _______,
   _______,  KC_INS,  KC_PSCR,   KC_APP,  QK_SWAP_HANDS_TOGGLE, QK_BOOT,             KC_PGUP, QK_CAPS_WORD_TOGGLE, AS_RPT, AS_DOWN, AS_UP, KC_BSPC,
   _______, KC_LCTL, KC_LALT,    KC_LGUI,    KC_LSFT,     KC_CAPS,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, QK_CLEAR_EEPROM,  KC_MPLY,       _______,  KC_PGDN, KC_HOME, XXXXXXX, KC_END,   XXXXXXX, _______,
+  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, QK_CLEAR_EEPROM,  KC_MUTE,       _______,  KC_PGDN, KC_HOME, XXXXXXX, KC_END,   XXXXXXX, _______,
                          _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 };
