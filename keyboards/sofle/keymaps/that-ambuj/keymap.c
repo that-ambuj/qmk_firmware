a34b2f93f6070a9418fbb626fefe79dd025b42c6
Ambuj Kumar <ambujs89@gmail.com>
2024-10-07 12:11:28 +0530

    reverse encoder direction for music controls

--- keyboards/sofle/keymaps/that-ambuj/keymap.c
+++ keyboards/sofle/keymaps/that-ambuj/keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),           ENCODER_CCW_CW(KC_MS_WH_DOWN, KC_MS_WH_UP) },
     [1] = { ENCODER_CCW_CW(_______, _______),           ENCODER_CCW_CW(_______, _______) },
     [2] = { ENCODER_CCW_CW(RGB_HUD, RGB_HUI),           ENCODER_CCW_CW(C(KC_PGDN), C(KC_PGUP)) },
-    [3] = { ENCODER_CCW_CW(KC_MNXT, KC_MPRV),           ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)}
+    [3] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT),           ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)}
 };
 #endif
 
