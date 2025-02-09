435dd44db31c85458a88f7dd3e9d909a4f487efe
Ambuj Kumar <ambujs89@gmail.com>
2025-02-09 12:38:44 +0530

    feat: add totem keymaps

---
+++ keyboards/totem/keymaps/default/config.h
@@ -0,0 +1,34 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+// #define MASTER_LEFT
+// #define MASTER_RIGHT
+
+#define EE_HANDS
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+//#define TAPPING_TERM 170
+#define TAPPING_TERM 180
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
+
+// Neutralize long hold of Alt and GUI keys
+#define DUMMY_MOD_NEUTRALIZER_KEYCODE KC_RIGHT_CTRL
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_ONESHOT
+
